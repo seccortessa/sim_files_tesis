69f4fcf9a00889050f87bdf48367064ba8bc6d67
Luisa Jojoa <lajojoam@unal.edu.co>
2020-11-13 20:03:22 -0500

    quitando tildes de intentble y agregando cambios de Luisa a Sebastian

--- leyendoimg.m
+++ leyendoimg.m
@@ -2,30 +2,30 @@ bebeimg = imread('bebeas.jpg');
 bebe2 = imread('redbaby.jpg');
 bebe3 = imread('bluebaby.jpg');
 
-red_i=flipud(bebeimg(:,:,1).');
-green_i=flipud(bebeimg(:,:,2).');
-blue_i=flipud(bebeimg(:,:,3).');
+% red_i=flipud(bebeimg(:,:,1).');
+% green_i=flipud(bebeimg(:,:,2).');
+% blue_i=flipud(bebeimg(:,:,3).');
 
 
-red = imresize(red_i,[750 483]);
-green = imresize(green_i,[750 483]) ;
-blue = imresize(blue_i,[750 483]) ;
+red = imresize(flipud(bebeimg(:,:,1).'),[750 483]);
+green = imresize(flipud(bebeimg(:,:,2).'),[750 483]) ;
+blue = imresize(flipud(bebeimg(:,:,3).'),[750 483]) ;
 
 
 
-red_i2=rot90(bebe2(:,:,1),2);
-green_i2=rot90(bebe2(:,:,2),2);
-blue_i2=rot90(bebe2(:,:,3),2);
+% red_i2=rot90(bebe2(:,:,1),2);
+% green_i2=rot90(bebe2(:,:,2),2);
+% blue_i2=rot90(bebe2(:,:,3),2);
 
-red2 = imresize(red_i2,[750 483]);
-green2 = imresize(green_i2,[750 483]) ;
-blue2 = imresize(blue_i2,[750 483]) ;
+red2 = imresize(rot90(bebe2(:,:,1),2),[750 483]);
+green2 = imresize(rot90(bebe2(:,:,2),2),[750 483]) ;
+blue2 = imresize(rot90(bebe2(:,:,3),2),[750 483]) ;
 
 
-red_i3=rot90(bebe3(:,:,1),2);
-green_i3=rot90(bebe3(:,:,2),2);
-blue_i3=rot90(bebe3(:,:,3),2);
+% red_i3=rot90(bebe3(:,:,1),2);
+% green_i3=rot90(bebe3(:,:,2),2);
+% blue_i3=rot90(bebe3(:,:,3),2);
 
-red3 = imresize(red_i3,[750 483]);
-green3 = imresize(green_i3,[750 483]) ;
-blue3 = imresize(blue_i3,[750 483]) ;
+red3 = imresize(rot90(bebe3(:,:,1),2),[750 483]);
+green3 = imresize(rot90(bebe3(:,:,2),2),[750 483]) ;
+blue3 = imresize(rot90(bebe3(:,:,3),2),[750 483]) ;
